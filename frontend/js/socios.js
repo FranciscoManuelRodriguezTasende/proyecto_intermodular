b7c7d1b5c8615d38e7027295dc7f1a55942f0cdf
FranciscoManuelRodriguezTasende <frodriguezt24@fpcoruna.afundacion.org>
2026-05-28 13:29:32 +0200

    Implementación diseño responsive en la página socios

--- frontend/js/socios.js
+++ frontend/js/socios.js
@@ -255,6 +255,16 @@ document.addEventListener('DOMContentLoaded', () => {
         } catch (error) { console.error(error); }
     };
 
+    // FUNCIONALIDAD DEL MENÚ HAMBURGUESA
+    const btnMenu = document.getElementById('btn-menu-movil');
+    const menuNav = document.querySelector('.menu-nav');
+
+    if (btnMenu && menuNav) {
+        btnMenu.addEventListener('click', () => {
+            menuNav.classList.toggle('mostrar-menu');
+        });
+    }
+    
     // --- 10. INICIAR ---
     cargarSocios();
 });
